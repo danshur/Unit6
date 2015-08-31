69b1a6eb5584abd389d9f0d5230be8ad94d667ad
danshur <danshur@gmail.com>
2015-08-31 15:37:18 -0700

    finished talking beetle

--- src/TalkingBeetle.java
+++ src/TalkingBeetle.java
@@ -1,24 +1,33 @@
 /*
  * Exercise	
-1. Create	a	new	class	TalkingBeetle.	The	TalkingBeetle	prints	a	message	when	it	is	
-about	to	make	a	circle.	For	example,	it	would	say	“Watch	out,	I’m	making	a	circle”	
-before	turning.	Test	your	work.	
-2. Add	a	constructor	to	your	class	that	takes	a	color	(String)	and	a	name	(String)	as	
-parameters.	The	name	should	be	an	instance	variable.	Remember	that	the	
-superclass	already	has	a	constructor	that	takes	color	as	a	parameter.	
+1. Create a	new	class TalkingBeetle. The TalkingBeetle prints a	message	when it is 
+about to make a	circle.	For	example, it	would say “Watch out, I’m making a circle”	
+before	turning. Test your work.	
+2. Add	a constructor to your class	that takes a color	(String)	and	a	name	(String)	as	
+parameters.	The	name should	be	an	instance	variable.	Remember	that	the	
+superclass	already	has	a constructor	that	takes	color	as	a	parameter.	
 3. Update	the	method	makeCircle	so	that	it	now	says	“Name	is	about	to	make	a	
 circle”.
  */
 
 public class TalkingBeetle extends Beetle {
-	public String name = this.name;
-	
+	private String name;
+
 	public TalkingBeetle(String color, String name) {
-		//this.name = Beetle(name);
+		super(color);
+		this.setName(name);
 	}
 
 	public void makeCircle(double radius) {
-		 System.out.println("Watch out, I'm making a circle");
+		 System.out.println(this.getName() + " is about to make a circle");
 		 super.makeCircle(radius);
-		 } 
+		 }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	} 
 }
--- src/TestTalkingBeetle.java
+++ src/TestTalkingBeetle.java
@@ -3,8 +3,11 @@ public class TestTalkingBeetle {
 
 	public static void main(String[] args) {
 		//test talking beetle
-		 TalkingBeetle noisy = new TalkingBeetle();
-		 noisy.makeCircle(20);
+		 TalkingBeetle noisy = new TalkingBeetle("red", "Noisyyy");
+		 TalkingBeetle alsonoisy = new TalkingBeetle("green", "AlsoNoisyyy");
+		 //System.out.println("Your talking turtle name is " + noisy.getName());
+		 noisy.makeCircle(10);
+		 alsonoisy.makeCircle(20);
 	}
 
 }
